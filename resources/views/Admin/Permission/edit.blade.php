f18f2fd69296a7c92a6122b6ec3c1be5779298cf
trungdinh96 <109255230+trungdinh96@users.noreply.github.com>
2023-04-26 16:12:38 +0700

    update

--- resources/views/Admin/Permission/edit.blade.php
+++ resources/views/Admin/Permission/edit.blade.php
@@ -96,7 +96,7 @@
     <script>
         $('.checkbox_wrapper').on('click', function()
         {
-$(this).parents('.card').find('.checkbox_childrent').prop('checked', $(this).prop('checked'));
+          $(this).parents('.card').find('.checkbox_childrent').prop('checked', $(this).prop('checked'));
         });
     </script>
 
